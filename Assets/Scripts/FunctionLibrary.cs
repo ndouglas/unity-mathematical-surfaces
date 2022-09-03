c997c2ac296d512be3be678c97def4d4ba8543ea
Nathan Douglas <github@darkdell.net>
2022-09-03 12:47:27 -0400

    Twisty Sphere.

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,47 +3,63 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
 
-    public delegate float Function (float x, float z, float t);
+    public delegate Vector3 Function (float u, float v, float t);
 
     public enum FunctionName { 
         Wave, 
         MultiWave, 
-        SlidingMultiWave,
         Ripple,
+        Sphere,
     }
 
 	static Function[] functions = {
         Wave,
         MultiWave,
-        SlidingMultiWave,
         Ripple,
+        Sphere,
     };
 
     public static Function GetFunction (FunctionName name) {
 		return functions[(int)name];
 	}
 
-    public static float Wave (float x, float z, float t) {
-        return Sin(PI * (x + z + t));
-    }
-
-	public static float MultiWave (float x, float z, float t) {
-		float y = Sin(PI * (x + t));
-        y += 0.5f * Sin(2f * PI * (z + t));
-		return y * (2f / 3f);
+	public static Vector3 Wave (float u, float v, float t) {
+		Vector3 position;
+		position.x = u;
+		position.y = Sin(PI * (u + v + t));
+		position.z = v;
+		return position;
 	}
 
-	public static float SlidingMultiWave (float x, float z, float t) {
-		float y = Sin(PI * (x + 0.5f * t));
-		y += 0.5f * Sin(2f * PI * (z + t));
-		y += Sin(PI * (x + z + 0.25f * t));
-		return y * (1f / 2.5f);
+	public static Vector3 MultiWave (float u, float v, float t) {
+		Vector3 position;
+		position.x = u;
+		position.y = Sin(PI * (u + 0.5f * t));
+		position.y += 0.5f * Sin(2f * PI * (v + t));
+		position.y += Sin(PI * (u + v + 0.25f * t));
+		position.y *= 1f / 2.5f;
+		position.z = v;
+		return position;
 	}
 
-	public static float Ripple (float x, float z, float t) {
-		float d = Sqrt(x * x + z * z);
-		float y = Sin(PI * (4f * d - t));
-		return y / (1f + 10f * d);
+	public static Vector3 Ripple (float u, float v, float t) {
+		float d = Sqrt(u * u + v * v);
+		Vector3 position;
+		position.x = u;
+		position.y = Sin(PI * (4f * d - t));
+		position.y /= 1f + 10f * d;
+		position.z = v;
+		return position;
+	}
+
+	public static Vector3 Sphere (float u, float v, float t) {
+		Vector3 position;
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+        float s = r * Cos(0.5f * PI * v);
+		position.x = s * Sin(PI * u);
+		position.y = r * Sin(0.5f * PI * v);
+		position.z = s * Cos(PI * u);
+		return position;
 	}
 
 }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -15,19 +15,10 @@ public class Graph : MonoBehaviour {
 
 	void Awake () {
 		float step = 2f / resolution;
-		var position = Vector3.zero;
 		var scale = Vector3.one * step;
         points = new Transform[resolution * resolution];
-		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-			if (x == resolution) {
-				x = 0;
-                z += 1;
-			}
+		for (int i = 0; i < points.Length; i++) {
 			Transform point = points[i] = Instantiate(pointPrefab);
-			position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-			position.y = position.x;
-			point.localPosition = position;
 			point.localScale = scale;
             point.SetParent(transform, false);
 		}
@@ -36,12 +27,17 @@ public class Graph : MonoBehaviour {
 	void Update () {
         float time = Time.time;
         FunctionLibrary.Function func = FunctionLibrary.GetFunction(function);
-		for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-			position.y = func(position.x, position.z, time);
-            point.localPosition = position;
-        }
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution) {
+				x = 0;
+				z += 1;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			v = (z + 0.5f) * step - 1f;
+			points[i].localPosition = func(u, v, time);
+		}
 	}
 
 }
